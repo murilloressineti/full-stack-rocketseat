b1eff2480ae180efab923d3dbe8b5d798fc3acb6
Murillo Ressineti <murillobr22@gmail.com>
2025-08-22 17:47:57 -0300

    updated orders

--- node-js/iniciando-com-node-js/api-restaurant/src/routes/index.ts
+++ node-js/iniciando-com-node-js/api-restaurant/src/routes/index.ts
@@ -3,11 +3,12 @@ import { Router } from "express";
 import { tablesSessionsRoutes } from "./tables-sessions-routes";
 import { productsRoutes } from "./products-routes";
 import { tablesRoutes } from "./tables-routes";
-
+import { ordersRoutes } from "./orders-routes";
 
 const routes = Router();
 routes.use("/tables-sessions", tablesSessionsRoutes);
 routes.use("/products", productsRoutes);
 routes.use("/tables", tablesRoutes);
+routes.use("/orders", ordersRoutes);
 
 export { routes };
