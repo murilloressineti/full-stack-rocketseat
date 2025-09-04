d246cdbf7f1a97315a6ebfb023c061dfee2cde3a
Murillo Ressineti <murillobr22@gmail.com>
2025-09-04 14:51:00 -0300

    authentication and authorization

---
+++ node-js/node-js-com-containers/auth/src/routes/products.routes.ts
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import { ProductsController } from "@/controllers/products-controller";
+
+import { ensureAuthenticated } from "@/middlewares/ensureAuthenticated";
+import { verifyUserAuthorization } from "@/middlewares/verifyUserAuthorization";
+
+const productsRoutes = Router();
+const productsController = new ProductsController();
+
+productsRoutes.get("/", productsController.index);
+productsRoutes.post(
+  "/",
+  ensureAuthenticated,
+  verifyUserAuthorization(["sale", "admin"]),
+  productsController.create
+);
+
+export { productsRoutes };
