46da47467b38a3003a00fa7889b778b73de9ce8d
Murillo Ressineti <murillobr22@gmail.com>
2025-09-15 17:29:06 -0300

    upload refund project

--- node-js/bônus-api-do-refund-2.0/refund-api/src/routes/refunds-routes.ts
+++ node-js/bônus-api-do-refund-2.0/refund-api/src/routes/refunds-routes.ts
@@ -12,4 +12,16 @@ refundsRoutes.post(
   refundsController.create
 );
 
+refundsRoutes.get(
+  "/",
+  verifyUserAuthorization(["manager"]),
+  refundsController.index
+);
+
+refundsRoutes.get(
+  "/:id",
+  verifyUserAuthorization(["employee", "manager"]),
+  refundsController.show
+)
+
 export { refundsRoutes };
