9dca81af4e1fbda4d33c1773bd1b86cca6431844
Murillo Ressineti <murillobr22@gmail.com>
2025-09-18 12:40:10 -0300

    Team Management

--- node-js/node-js-com-containers/desafios/task-manager/src/routes/teams-routes.ts
+++ node-js/node-js-com-containers/desafios/task-manager/src/routes/teams-routes.ts
@@ -1,5 +1,4 @@
 import { Router } from "express";
-
 import { TeamsController } from "@/controllers/teams-controller";
 import { ensureAuthenticated } from "@/middlewares/ensure-authenticated";
 import { verifyUserAuthorization } from "@/middlewares/verifyUserAuthorization";
@@ -7,7 +6,42 @@ import { verifyUserAuthorization } from "@/middlewares/verifyUserAuthorization";
 const teamsRoutes = Router();
 const teamsController = new TeamsController();
 
-teamsRoutes.use(ensureAuthenticated, verifyUserAuthorization(["admin"]));
-teamsRoutes.post("/", teamsController.create);
+teamsRoutes.use(ensureAuthenticated);
+
+teamsRoutes.get(
+  "/",
+  verifyUserAuthorization(["admin", "member"]),
+  teamsController.index
+);
+
+teamsRoutes.post(
+  "/",
+  verifyUserAuthorization(["admin"]),
+  teamsController.create
+);
+
+teamsRoutes.put(
+  "/:id",
+  verifyUserAuthorization(["admin"]),
+  teamsController.update
+);
+
+teamsRoutes.delete(
+  "/:id",
+  verifyUserAuthorization(["admin"]),
+  teamsController.delete
+);
+
+teamsRoutes.post(
+  "/:id/members",
+  verifyUserAuthorization(["admin"]),
+  teamsController.addMember
+);
+
+teamsRoutes.delete(
+  "/:id/members/:userId",
+  verifyUserAuthorization(["admin"]),
+  teamsController.removeMember
+);
 
 export { teamsRoutes };
