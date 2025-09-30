fe24c4d029520973fc4ea466aaa4e42fceedb8b2
Murillo Ressineti <murillobr22@gmail.com>
2025-09-30 17:44:19 -0300

    update users

--- react/avançando-no-react/refund-2.0/web/src/pages/SignUp.tsx
+++ react/avançando-no-react/refund-2.0/web/src/pages/SignUp.tsx
@@ -1,7 +1,27 @@
 import { useState } from "react";
+import { z, ZodError } from "zod";
+import { AxiosError } from "axios";
+import { useNavigate } from "react-router";
+
+import { api } from "../services/api";
 
 import { Input } from "../components/Input";
 import { Button } from "../components/Button";
+
+const signUpSchema = z
+  .object({
+    name: z.string().trim().min(1, { message: "Informe o nome" }),
+    email: z.string().email({ message: "E-mail inválido" }),
+    password: z
+      .string()
+      .min(6, { message: "Senha deve ter pelo menos 6 dígitos" }),
+
+    passwordConfirm: z.string({ message: "Confirme a senha" }),
+  })
+  .refine((data) => data.password === data.passwordConfirm, {
+    message: "As senhas não são iguais",
+    path: ["passwordConfirm"],
+  });
 
 export function SignUp() {
   const [name, setName] = useState("");
@@ -10,10 +30,41 @@ export function SignUp() {
   const [passwordConfirm, setPasswordConfirm] = useState("");
   const [isLoading, setIsLoading] = useState(false);
 
-  function onSubmit(e: React.FormEvent) {
+  const navigate = useNavigate();
+
+  async function onSubmit(e: React.FormEvent) {
     e.preventDefault();
 
-    console.log(name, email, password, passwordConfirm);
+    try {
+      setIsLoading(true);
+
+      const data = signUpSchema.parse({
+        name,
+        email,
+        password,
+        passwordConfirm,
+      });
+
+      await api.post("/users", data);
+
+      if (confirm("Cadastrado com sucesso. Ir para tela de entrar?")) {
+        navigate("/");
+      }
+    } catch (error) {
+      console.log(error);
+
+      if (error instanceof ZodError) {
+        return alert(error.issues[0].message);
+      }
+
+      if(error instanceof AxiosError) {
+        return alert(error.response?.data.message)
+      }
+
+      alert("Não foi possível cadastrar!");
+    } finally {
+      setIsLoading(false);
+    }
   }
 
   return (
