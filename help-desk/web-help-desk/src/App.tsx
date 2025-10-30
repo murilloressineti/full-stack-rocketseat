8bd313397683ae65e444c6540847e6a39880d317
Murillo Ressineti <murillobr22@gmail.com>
2025-10-30 17:14:56 -0300

    feat: component construction icon

--- help-desk/web-help-desk/src/App.tsx
+++ help-desk/web-help-desk/src/App.tsx
@@ -1,9 +1,15 @@
 import Text from "./components/text";
+import Icon from "./components/icon";
+import BanIcon from "./assets/icons/ban.svg?react";
 
 export default function App() {
   return (
-    <Text as="h1" size="xxs" className="text-feedback-danger">
-      Olá, mundo!
-    </Text>
+    <div>
+      <Text as="h1" weight="bold" className="text-blue-dark!">
+        Olá, mundo!
+      </Text>
+
+      <Icon svg={BanIcon} animate className="fill-feedback-danger"/>
+    </div>
   );
 }
